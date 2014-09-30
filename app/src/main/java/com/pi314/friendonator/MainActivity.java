39486aec1134566ea6ca9222aea97fa7f2595c62
Andrea Tupini <tupini07@gmail.com>
2014-09-30 16:34:58 -0600

    agregado base bluetoot

--- app/src/main/java/com/pi314/friendonator/MainActivity.java
+++ app/src/main/java/com/pi314/friendonator/MainActivity.java
@@ -1,20 +1,24 @@
 package com.pi314.friendonator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import Bluetooth.BlueMan;
+
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent ine = new Intent(MainActivity.this, BlueMan.class);
+        startActivity(ine);
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
