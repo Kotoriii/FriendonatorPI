e0fff6a3e082a59898c18ae499f58e2f91dd604c
Cristian Oviedo <cristianoviedo.07@gmail.com>
2014-10-15 09:32:35 -0600

    Insercion del menu

---
+++ app/src/main/java/com/pi314/friendonator/Item_objct.java
@@ -0,0 +1,25 @@
+package com.pi314.friendonator;
+
+/**
+ * Created by grecia on 15/10/2014.
+ */
+public class Item_objct {
+    private String titulo;
+    private int icono;
+    public Item_objct(String title, int icon) {
+        this.titulo = title;
+        this.icono = icon;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getIcono() {
+        return icono;
+    }
+    public void setIcono(int icono) {
+        this.icono = icono;
+    }
+}
